9264e8a5f41f90e8b7a0fc59996e635907bfb378
Igor Dinamsky <dinamsky@gmail.com>
2022-06-12 11:09:00 +0300

    all

---
+++ plutoneon-reactjs/src/assets/fake-data/data-slider-2.js
@@ -0,0 +1,40 @@
+import img1 from '../images/slider/slide_2.png';
+import img2 from '../images/slider/slide_2.png';
+import imgbg1 from '../images/slider/bg_slide_2.png'
+import imgbg2 from '../images/slider/bg_slide_2.png'
+
+
+const heroSliderData = [
+    {
+        title_1: "Открывайте и собирайте",
+        title_2: "необычные",
+        title_3: "Monster ",
+        title_4: "NFTs",
+        description: "Торговая площадка для коллекций персонажей монстров NFT с незаменяемыми токенами",
+        img: img1,
+        imgbg: imgbg1,
+        class: 'left'
+    },
+    {
+        title_1: "Discover, find,",
+        title_2: "Sell extraordinary",
+        title_3: "Monster ",
+        title_4: "NFTs",
+        description: "Marketplace for monster character cllections non fungible token NFTs",
+        img: img2,
+        imgbg: imgbg2,
+        class: 'center'
+    },
+    {
+        title_1: "Discover, find,",
+        title_2: "Sell extraordinary",
+        title_3: "Monster ",
+        title_4: "NFTs",
+        description: "Marketplace for monster character cllections non fungible token NFTs",
+        img: img2,
+        imgbg: imgbg2,
+        class: 'right'
+    },
+]
+
+export default heroSliderData;
---
+++ plutoneon-reactjs/src/assets/fake-data/data-slider.js
@@ -0,0 +1,38 @@
+import img1 from '../images/slider/slide_1.png';
+import img2 from '../images/slider/slide_1.png';
+import imgbg1 from '../images/slider/bg_slide_1.png'
+import imgbg2 from '../images/slider/bg_slide_1.png'
+
+
+const heroSliderData = [
+    {
+        title_1: "Узнавайте, Ищите,",
+        title_2: "Продавайте",
+        title_3: "Создавайте NFT",
+        description: "Маркетплэйс для коллекций NFT",
+        img: img1,
+        imgbg: imgbg1,
+        class:'right'
+    },
+    {
+        title_1: "Узнавайте, Ищите,",
+        title_2: "Продавайте",
+        title_3: "Создавайте NFT",
+        description: "Маркетплэйс для коллекций NFT",
+        img: img1,
+        imgbg: imgbg1,
+        class:'right'
+    },
+    {
+        title_1: "Узнавайте, Ищите,",
+        title_2: "Продавайте",
+        title_3: "Создавайте NFT",
+        description: "Маркетплэйс для коллекций NFT",
+        img: img1,
+        imgbg: imgbg1,
+        class:'left'
+    },
+    
+]
+
+export default heroSliderData;
